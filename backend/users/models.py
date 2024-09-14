02245afe4d6351f824f2601e8a7f3b2e6fb69562
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-14 15:30:49 +0300

    change model user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,33 +1,21 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError('Username is required')
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+class User(AbstractUser):
+  email = models.EmailField(unique=True, blank=True, null=True)
+  first_name = models.CharField(max_length=50, blank=True, null=True)
+  last_name = models.CharField(max_length=50, blank=True, null=True)
 
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = []
 
-        return self.create_user(username, password, **extra_fields)
+  def save(self, *args, **kwargs):
+    if self.username:
+      if self.pk and User.objects.filter(username=self.username).exclude(pk=self.pk).exists():
+        raise ValidationError('Пользователь с таким username уже существует.')
+    super().save(*args, **kwargs)
 
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(blank=True, null=True)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+  def __str__(self):
+      return self.username
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
-
-    def __str__(self):
-        return self.username
